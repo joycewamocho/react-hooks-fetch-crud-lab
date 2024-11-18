1bb4da57a99931e446811ec92a7d3fc6926ff870
joyce wamocho <joywamo@gmail.com>
2024-11-18 15:06:02 +0300

    crud lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const[questions,setQuestions]=useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((res)=> res.json())
+    .then((data)=> setQuestions(data))
+  },[])
+
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  function handleDelete(id){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        // Filter out the deleted question from state
+        const updatedQuestions = questions.filter((question) => question.id !== id);
+        setQuestions(updatedQuestions);
+      })
+  }
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddQuestion={handleAddQuestion} /> : <QuestionList questions={questions}  onDelete={handleDelete}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question,onDelete}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,6 +9,9 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteClick(){
+    onDelete(id)
+  }
   return (
     <li>
       <h4>Question {id}</h4>
@@ -17,7 +20,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
